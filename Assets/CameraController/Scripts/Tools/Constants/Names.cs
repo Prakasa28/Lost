8a2cd82d59eac1f3811c5fd71f475b0c68cea7dc
Prakasa <i450220@fhict.nl>
2021-10-11 00:00:23 +0200

    Level 1 completed

--- Assets/CameraController/Scripts/Tools/Constants/Names.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace ССP.Tools.Constants
-{
-    public static class Names
-    {
-        public const string ScrollAxis = "Mouse ScrollWheel", CameraTarget = "_ccp Target", CameraClone = "_camera Clone", Player = "Player";
-    }
-}
