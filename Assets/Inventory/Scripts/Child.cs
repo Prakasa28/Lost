bedc4eb3b7f95952035298b05e1db24fd550fde2
Alex Petrov <aleks.ppetrov2000@gmail.com>
2021-10-21 05:30:24 +0200

    child functionality and animations

--- Assets/Inventory/Scripts/Child.cs
+++ Assets/Inventory/Scripts/Child.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,9 +8,17 @@ public class Child : MonoBehaviour
 {
     private GameObject player;
     public float moveSpeed = 5; //move speed
+    public float followRange = 2; //follow range
+    private float distance; //distance between players
     private Transform playerPos;
-
     private Animator animator;
+    private int isFollowingHash;
+    private int isDuyingHash;
+    public float timeRemaining = 10;
+    public GameObject portalEffect;
+    private bool isSpawned = false;
+    private bool canMove = true;
+    private GameObject newPortal;
 
     // Start is called before the first frame update
     void Start()
@@ -19,20 +28,90 @@ public class Child : MonoBehaviour
         playerPos = player.transform;
         // set the animator
         animator = GetComponent<Animator>();
+        isFollowingHash = Animator.StringToHash("IsFollowing");
+        isDuyingHash = Animator.StringToHash("IsSucked");
+        // get portal placeholder object
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveToPlayer();
+        if (canMove)
+            moveToPlayer();
+        
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            if (!isSpawned)
+                StartCoroutine(spawnPortal());
+            
+        }
     }
 
 
     void moveToPlayer()
     {
-        //move towards the player
-        transform.position = Vector3.MoveTowards(transform.position, playerPos.position, moveSpeed * Time.deltaTime);
-        //rotate to look at the player
-        transform.LookAt(playerPos);
+        var playerPositionToCheck = playerPos;
+        var ourPosition = transform;
+
+        distance = Vector3.Distance(playerPositionToCheck.position, ourPosition.position);
+
+        if (distance > followRange - 1)
+        {
+            //move towards the player
+            transform.position =
+                Vector3.MoveTowards(transform.position, playerPos.position, moveSpeed * Time.deltaTime);
+            //rotate to look at the player
+            transform.LookAt(playerPos);
+            // set animation on
+            animator.SetBool(isFollowingHash, true);
+        }
+        else
+        {
+            animator.SetBool(isFollowingHash, false);
+        }
+    }
+
+
+    IEnumerator spawnPortal()
+    {
+        isSpawned = true;
+        canMove = false;
+        Vector3 spawnPosition = transform.position;
+        spawnPosition.y += .4f;
+        newPortal = Instantiate (portalEffect, spawnPosition, Quaternion.Euler(new Vector3(90, 0, 0)));
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(SuckPlayer());
+        
+        
+
+    }
+
+    IEnumerator SuckPlayer()
+    {
+        float suckingTime = 10;
+        animator.SetBool(isFollowingHash,false);
+        animator.SetBool(isDuyingHash,true);
+        while (true)
+        {
+            if (suckingTime <= 0)
+                break;
+            
+            suckingTime -= Time.deltaTime;
+            transform.position += new Vector3(0, -0.2f, 0) * Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject);
+        Destroy(newPortal);
+        animator.SetBool(isDuyingHash,false);
+        
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, followRange);
     }
 }
