db380aae624b7e7c417ee2daef93178e56189e26
Alex Petrov <aleks.ppetrov2000@gmail.com>
2021-11-09 19:43:33 +0100

    fix sizes of prefabs

--- Assets/Joro/UI/Bilboard.cs
+++ Assets/Joro/UI/Bilboard.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bilboard : MonoBehaviour
 {
-    public Camera _camera;
+    private Camera cameraPos;
+
+    private GameObject camera;
     // Update is called once per frame
+
+    private void Awake()
+    {
+        camera = GameObject.FindGameObjectWithTag("Camera");
+        cameraPos = camera.gameObject.GetComponent<Camera>();
+    }
+
     void LateUpdate()
     {
-       transform.forward = _camera.transform.forward; 
+        transform.forward = cameraPos.transform.forward;
     }
 }
